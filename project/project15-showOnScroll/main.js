f32ac74d9be81d8b19bc5018688e80775c86db13
ThangNV2k2 <thang08052002@gmail.com>
2023-07-30 20:11:04 +0700

    update project15

---
+++ project/project15-showOnScroll/main.js
@@ -0,0 +1,24 @@
+let animationElements = document.querySelectorAll('.show-on-scroll')
+
+function toggleAnimation(element) {
+    let rect = element.getClientRects()[0]
+    let heightScreen = window.innerHeight
+
+    //check xem element có năm trong màn hình hay không
+    if(!(rect.bottom < 0 ||rect.top > heightScreen)) {
+        //trong màn hình
+        element.classList.add('start')
+    } else {
+        element.classList.remove('start')
+    }
+}
+
+function checkAnimation() {
+    animationElements.forEach((el) => {
+        toggleAnimation(el)
+        console.log(el)
+    })
+}
+
+window.onscroll = checkAnimation
+checkAnimation()
